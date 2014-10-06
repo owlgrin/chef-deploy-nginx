cc5770014f7c1816693054d1b65aa22a89258b29
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-06 13:46:32 +0530

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'horntell-nginx'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures horntell-nginx'
+long_description 'Installs/Configures horntell-nginx'
+version          '0.1.0'
+
